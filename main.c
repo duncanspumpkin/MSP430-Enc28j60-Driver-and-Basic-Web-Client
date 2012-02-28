6021866ecb5eef60a13b42e0ea8b42b142d4b262
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2012-02-28 14:23:40 +0000

    Removed redundant commented out code.

--- main.c
+++ main.c
@@ -2,14 +2,12 @@
 #include "enc28j60.h"
 #include <string.h>
 
-#define SEL_MAC(x)  (x==1) ? (P2OUT&=(~BIT0)) : (P2OUT|=BIT0)  
-
-//Switch the layout for the enc28j60
-
+// Switch to host order for the enc28j60
 #define HTONS(x) ((x<<8)|(x>>8))
 
 unsigned char uip_buf[250];
 unsigned char uip_len;
+// Mac address of the enc28j60
 const unsigned char bytMacAddress[6] = {0x00,0xa0,0xc9,0x14,0xc8,0x00};
 
 typedef struct
@@ -50,10 +48,7 @@ void PrepArp()
   arpPacket.opCode = HTONS(0x0001);
   
   memcpy(&arpPacket.senderMAC[0],&bytMacAddress[0],sizeof(bytMacAddress));
-//  for(int i = 0; i < 4; ++i)
-//  {
-//    arpPacket.senderIP[i] = 0x00;
-//  }
+
   arpPacket.senderIP[0] = 192;
   arpPacket.senderIP[1] = 168;
   arpPacket.senderIP[2] = 0;
@@ -92,8 +87,7 @@ int main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW + WDTHOLD;
- // DCOCTL = CALDCO_16MHZ;
-  //BCSCTL1 = CALBC1_16MHZ;
+
   P1DIR = 0x01;
   P1OUT = 0x0;
   initMAC();
@@ -117,62 +111,6 @@ int main( void )
         P1OUT = 0x1;
       }
     }
-    //PrepArp();
-//    
-//    ARP* arp = (ARP*)&uip_buf[sizeof(EtherNetII)];
-//    
-//    if( eth->type[0] == 0x08 && eth->type[1] == 0x06 && eth->DestAddrs[0]!= 0xff)
-//    {
-//      P1OUT = 0x1;
-////      EtherNetII eth2;
-////      ARP arp2;
-////      memcpy(&(eth->DestAddrs[0]),&eth2.DestAddrs[0],6);
-////      memcpy(&(eth->DestAddrs[0]),&arp2.targetMAC[0],6);
-////      memcpy(&(arp->senderIP[0]),&arp2.targetIP[0],4);
-////      
-////      memcpy(&eth2.SrcAddrs[0],&bytMacAddress[0],sizeof(bytMacAddress));
-////      eth2.type[0] = 0x08;
-////      eth2.type[1] = 0x06;
-//      
-//      PrepArp();
-//    }
+   
   }
-  //return 0;
-
-  
-//  initSPI();  
-//  unsigned char opcode = 0xff;
-//  SEL_MAC(1);
-//  SPIWrite(&opcode,1);
-//  SEL_MAC(0);
-////   temp = ReadETHReg(ECON1);       // Read ECON1 0x1f register
-////  temp &= ~ECON1_BSEL;            // mask off the BSEL 3 bits
-////  temp |= bank;                   // set BSEL bits
-////  WriteCtrReg(ECON1, temp);       // write new values back to ENC28J60
-////   bytAddress |= WCR_OP;       // Set opcode 0x40
-////  SEL_MAC(TRUE);              // ENC CS low
-////  SPIWrite(&bytAddress,1);    // Tx opcode and address
-////  SPIWrite(&bytData,1);       // Tx data
-////  SEL_MAC(FALSE);
-//  unsigned char bytData;
-//  unsigned char bytAddress = 0x1F;
-//  SEL_MAC(1);                 // ENC CS low
-//  SPIWrite(&bytAddress,1);       // write opcode
-//  SPIRead(&bytData, 1);          // read value
-//  SEL_MAC(0);
-//  bytAddress = 0x1f | 0x40;
-//  bytData &= ~3;
-//  bytData |= 3;
-//  SEL_MAC(1);                 // ENC CS low
-//  SPIWrite(&bytAddress,1);       // write opcode
-//  SPIWrite(&bytData, 1);         // write value
-//  SEL_MAC(0);
-//  
-//  bytAddress = 0x1f;
-//  SEL_MAC(1);                 // ENC CS low
-//  SPIWrite(&bytAddress,1);       // write opcode
-//  SPIRead(&bytData, 1);          // read value
-//  SEL_MAC(0);
-  
-  //return 0;
-}
+ }
